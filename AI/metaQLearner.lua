9d5391def12c65ffebcb05f5ec33adce7a24a5b4
Patrick Klein <klein.pat@icloud.com>
2017-11-18 18:38:17 -0800

    Added basic metaQLearner functionality

--- AI/metaQLearner.lua
+++ AI/metaQLearner.lua
@@ -2,16 +2,22 @@
 --[[
   class for optimizing hyper-parameters of qLearner
 
+  --keys in params must return a table of values, e.g.
+  --    params = {numberOfLayers={2,3,4},
+  --              numHiddenNodes={256,512,1024},
+  --              }
+
+  -- more param options?
+  --  include dropout
+  --  backup
+  --  reload
+
+  -- meta options?
+  --  load/save memory
+  --  load pretrained network & finetune?
+
   Class Methods
-    __init(net, game)
-    train()
-    qLearn()
-    optimizeNet(batchInputs,batchTargets,actionVals)
-    selfEvaluate()
-    drawBars(score,divStep,maxScore)
-    updateConstants()
-    process(input)
-    save
+  ...
 
 ]]
 
@@ -28,93 +34,91 @@ require 'AI/qLearner'
 --globals
 
 
--- declare qLearner class
+-- declare metaQLearner class
 local metaQLearner, parent = torch.class('metaQLearner', 'AI')
 
 
 -- initialization function
-function metaQLearner:__init(game)
+-- input should be table for parameters
+function metaQLearner:__init(game, params)
 
   parent.__init(self, game)
 
+  --assert params for initialization
+  assert(params, 'No hyperparameters passed to metaQLearner.  Use qLearner instance if only default values desired.')
+  self.params = params
 
-  assert(game, 'No game assigned to AI!')
-
-  self.game = game
-  self.game.AI = self --turtles all the way down
-
-
-  numberOfLayers = nil
-
+  self.bestLearner = {}
+  self.bestScore = 0
+  self.bestConfig = {}
 
 end
 
 
-
--- public method for training neural network
+--method for finding optimal hyperparameters
 function metaQLearner:train()
-  assert(false, 'metaQLearner:train() method not implemented!')
+
+  --create first config
+  thisConfig = {}
+  for param_key, param_table in pairs(self.params) do
+    thisConfig[param_key] = param_table[1]
+  end
+
+  --initialize qLearner with config
+  thisLearner = qLearner(self.game.new(), thisConfig)
+  thisLearner:train()
+  thisScore = self:learnerEvaluate(thisLearner)
+
+  --save learner/score/config if best
+  if thisScore>self.bestScore then
+    --can't just clone learner, so technically creating new one with same config & net.
+    --this means it doesn't have the same replay memory, optimstate, etc.
+    thisNet = thisLearner.net:clone()
+    self.bestLearner = thisLearner.new(self.game.new(), thisConfig, thisNet)
+    self.bestConfig = thisConfig
+    self.bestScore = thisScore
+  end
+
+  print(self.bestConfig)
+
 end
+
+
+function metaQLearner:learnerEvaluate(learner)
+
+  local numTrials = 1e2
+  local runningTotal = 0
+  for myEval = 1, numTrials do
+    torch.manualSeed(42 * myEval)
+    runningTotal = runningTotal + learner.game:test()
+  end
+
+  torch.seed()
+  if self.game.name == '2048' then
+    score = runningTotal / numTrials
+  elseif self.game.name == 'TicTacToe' then
+    score = (1 / 2) * (1 + runningTotal / numTrials)
+  end
+
+  return score
+
+end
+
 
 -- method for running test trials
 function metaQLearner:selfEvaluate()
   assert(false, 'metaQLearner:selfEvaluate() method not implemented!')
+  return self.bestLearner:selfEvaluate()  --leaving behind assert, because is this even needed?
 end
+
 
 --public shorthand for forward pass model
 function metaQLearner:process(input)
-  assert(false, 'metaQLearner:process() method not implemented!')
+  return self.bestLearner:process(input)
 end
+
 
 function metaQLearner:save()
-  assert(false, 'metaQLearner:save() method not implemented!')
-end
-
-function metaQLearner:returnBestLearner()
-  assert(false, 'metaQLearner:returnBestLearner() method not implemented!')
-end
-
---create a new qLearner and assign parameters
-function metaQLearner:createQLearner()
-
-  local numHiddenNodes = 1024
-
-  net = nn.Sequential()
-
-  net:add(nn.Linear(self.game.numInputs, numHiddenNodes))
-  net:add(nn.ReLU())
-
-  net:add(nn.Linear(numHiddenNodes, numHiddenNodes))
-  net:add(nn.ReLU())
-
-  net:add(nn.Linear(numHiddenNodes, self.game.numOutputs))
-  --net:add(nn.AddConstant(2))
-
-  --do NOT assign self.game, need to create new instance
-  weakLearner = qLearner(self.game.new(), net)
-
-  --weak learner params
-  weakLearner.backup = false
-  weakLearner.loadMemory = false
-  weakLearner.saveMemory = false
-  weakLearner.reload = false
-  --weakLearner.verbose = false
-
-  weakLearner.numLoopsToFinish = 1000
-  weakLearner.numLoopsForLinear = 500
-  weakLearner.targetNetworkUpdateDelay = 100
-  weakLearner.replayStartSize = 2048
-  weakLearner.replaySize = 2048
-  weakLearner.batchSize = 2048
-  weakLearner.numTrainingEpochs = 5
-
-  weakLearner.eps_initial = 0.9
-  weakLearner.eps_final = 0
-  weakLearner.gamma_initial = 0.5
-  weakLearner.gamma_final = 0.5
-  weakLearner.learningRate = 0.00001
-  weakLearner:updateConstants()
-
-  return weakLearner
-
+  --Note: despite file name, saved object has class qLearner
+  torch.save('saves/metaQLearner_'..self.game.name..'.ai', self.bestLearner)
 end
